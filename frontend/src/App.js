2333b075801ab2ccbf6b179110ae0a232b04c4db
Chiipo <156424376+Chiipo@users.noreply.github.com>
2024-03-13 12:19:24 +0000

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import AddTodo from "./pages/AddTodo"
 import Home from "./pages/Home"
 import EditTodo from "./pages/EditTodo"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+
 const App = () => {
   return (
     <div>
@@ -10,19 +11,9 @@ const App = () => {
         <h1>Todo List</h1>
         <Navbar />
         <Routes>
-          <Route
-            path='/'
-            element={<Home />}
-          />
-          <Route
-            path='/'
-            element={<AddTodo />}
-          />
-          <Route
-            // dynamic :id to create custom route
-            path='/'
-            element={<EditTodo />}
-          />
+          <Route path='/' element={<Home />} />
+          <Route  path='/add-todo' element={<AddTodo />} />
+          <Route path='/edit/:id' element={<EditTodo />} />
         </Routes>
 
       </BrowserRouter>
