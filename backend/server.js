44634b57e1e6ec2e82e505a6d71a5198eca7ec75
mcnsgit <cardigamiguel221@gmail.com>
2024-03-13 14:47:33 +0000

    items api point fix

--- backend/server.js
+++ backend/server.js
@@ -13,15 +13,10 @@ app.use(cors());
 // Parse JSON requests
 app.use(express.json());
 app.use('/items', itemRoutes);
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((error) => console.error('Error connecting to MongoDB:', error))
-  .then(() => {
-    app.listen(4000, () => {
-      console.log('Server running on port 4000');
-    });
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
-  });
 
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(4000, () => console.log('Server running on port 4000'));
+  })
+  .catch((error) => console.error('Error connecting to MongoDB:', error));
