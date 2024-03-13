7bd8152da4f25bd801f84877eb32ba27149be978
MacaCodes <gibsonmacauley@gmail.com>
2024-03-13 15:55:52 +0000

    cardandhome
    fixed deletehandler

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,40 +1,44 @@
 import { useEffect, useState } from "react";
 import { readTodos } from "../api/readTodos";
-import Card from '../components/Card'
+import Card from "../components/Card";
 import { deleteTodo } from "../api/deleteTodo";
 
 const Home = () => {
+    const [todos, setTodos] = useState([]);
 
-const [todos, setTodos] = useState([])
+    const deleteHandler = async (todoId) => {
+        try {
+            await deleteTodo(todoId);
+            setTodos(prevTodos => prevTodos.filter(todo => todo._id !== todoId));
+        } catch (error) {
+            console.error("Error deleting todo:", error);
+        }
+    };
 
- const deleteHandler = async (todo) => {
-    try {
-    await deleteTodo(todoId); 
-    setTodos(prevTodos => prevTodos.filter(todo => todo._id !== todoId));
-    } catch (error) {
-        console.error("Error deleting todo:", error);
-    }
-};
-    }
     useEffect(() => {
         const fetchTodos = async () => {
-            let data = await readTodos()
-            setTodos(data.todos)
-            console.log(data.message)
-        }
-        fetchTodos()
-    }, [])
+            try {
+                let data = await readTodos();
+                setTodos(data.todos);
+                console.log(data.message);
+            } catch (error) {
+                console.error("Error fetching todos:", error);
+            }
+        };
+        fetchTodos();
+    }, []);
 
-    if (!todos) return <h1>loading...</h1>
+    if (!todos) return <h1>loading...</h1>;
+    
     return (
         <div>
             <>
-                {
-                    todos ? todos.map((todo) => <Card key={todo._id} deleteHandler={deleteHandler} todo={todo}/>)
-                        : <p>loading...</p>
-                }
+                {todos.map((todo) => (
+                    <Card key={todo._id} deleteHandler={deleteHandler} todo={todo} />
+                ))}
             </>
         </div>
     );
+};
 
-export default Home
+export default Home;
