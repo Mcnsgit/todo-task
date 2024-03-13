1b22dddf3f2c10b208b4a7486903060f4b65a7a6
Chiipo <156424376+Chiipo@users.noreply.github.com>
2024-03-13 11:24:15 +0000

    Update getTodo.js

--- frontend/src/api/getTodo.js
+++ frontend/src/api/getTodo.js
@@ -1,5 +1,7 @@
 const API_URL = `http://localhost:4000`
 
-export const getTodo = async (id) => {
-
+export const getTodos = async () => {
+    let response = await fetch('http://localhost:4000/todos/items')
+    let data = await response.json()
+    return data.todos
 }
